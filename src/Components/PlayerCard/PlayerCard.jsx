32cbcfb7f0afd125dce460bb5dbe0f167f372169
Tasha Stenner <Tasha.Stenner@Nology.local>
2020-05-07 12:14:28 +0100

    made-field

--- src/Components/PlayerCard/PlayerCard.jsx
+++ src/Components/PlayerCard/PlayerCard.jsx
@@ -2,21 +2,29 @@ import React from "react";
 import styles from "./PlayerCard.module.scss";
 import player from "../../Images/player.png";
 import Card from "react-bootstrap/Card";
-
+import Button from "react-bootstrap/Button";
 
 const PlayerCard = (props) => {
-const {name , position, dob, nationality} = props; 
+  const { name, position, dob, nationality } = props;
+  const convertedDob = dob.substring(0,10).replace("-", " ").split(" ").reverse().join(" ");
 
   return (
     <>
-      <Card style={{ width: "30vw", height: "40vh"  }} >
-        <Card.Img variant="top" src={player} style={{ width: "100%", height: "50%" }}/>
-          <Card.Body style={{ width: "80%", height: "100%"}}>
+      <Card style={{ width: "20vw", height: "40vh", color: "white", margin: "8px"}}>
+        <Card.Img
+          variant="top"
+          src={player}
+          style={{ width: "100%", height: "100%" }}
+        />
+        <Card.ImgOverlay>
+          <Card.Body style={{ width: "80%", height: "60%" , padding: "2px"}}>
             <Card.Title>{name}</Card.Title>
             <Card.Text>{`Position: ${position}`}</Card.Text>
-            <Card.Text>{`Date of Birth: ${dob}`}</Card.Text>
+            <Card.Text>{`Date of Birth: ${convertedDob}`}</Card.Text>
             <Card.Text>{`Nationality: ${nationality}`}</Card.Text>
+            <Button style={{color: "black", backgroundColor:"Yellow", border:"2px solid black", marginTop: "4px"}} >Select Player</Button>
           </Card.Body>
+        </Card.ImgOverlay>
       </Card>
     </>
   );
--- src/Containers/FantasyFootball/FantasyFootball.jsx
+++ src/Containers/FantasyFootball/FantasyFootball.jsx
@@ -4,16 +4,36 @@ import PlayerCard from "../../Components/PlayerCard";
 import Data from "../../Data/football-team.data.json";
 
 const FantasyFootball = () => {
+  let selectedPlayers = [];
+  let playerId = [];
+
   return (
     <>
-      {Data.map((player) => {
-        <PlayerCard
-          name={player.name}
-          position={player.position}
-          dob={player.dateOfBirth}
-          nationality={player.nationality}
-        />;
-      })}
+      <div className={styles.pitch}>
+        <section className={styles.selctionTable}>
+          {Data.map((player) => (
+            <PlayerCard
+              name={player.name}
+              position={player.position}
+              dob={player.dateOfBirth}
+              nationality={player.nationality}
+            />,
+          (playerId = [...playerId += player.id])
+          ))}
+        </section>
+        <section className={styles.mySelectedTeam}>
+          <h2>Select Your Team</h2>
+          <h3> Rules:</h3>
+          <ol>
+            <li>You can only have 1 Goal Keeper</li>
+            <li>You can only have 3 Defenders</li>
+            <li>You can only have 4 Midfielders</li>
+            <li>You can only have 3 Attackers</li>
+            <li>You can only select each player once</li>
+          </ol>
+        </section>
+        {console.log(playerId)}
+      </div>
     </>
   );
 };
